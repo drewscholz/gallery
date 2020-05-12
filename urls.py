7d9d4d8930c36d9c0e64644bb0c9e290fb33214d
Drew Scholz <51394014+drewscholz@users.noreply.github.com>
2020-05-12 07:19:16 -0700

    speed ups and pagination

--- urls.py
+++ urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path(r'', views.directory, name='directory'),
-    path('img_select/<str:img>/', views.select, name='img_select')
+    path('select/<str:img>/', views.select, name='select'),
+    path('page/<str:page>/', views.page, name='page')
 ]
--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.http import Http404
 
 from geodude.settings import STATIC_ROOT
@@ -11,25 +12,66 @@ import io
 import time
 import numpy as np
 import cv2
+import math
+
+
+class ImageState():
+    TOTAL_IMG_NUM = 0
+    IMAGES = []
+    CURRENT_PAGE = 1
+    IMAGES_PER_PAGE = 6
+
+img_state = ImageState()
+
 
 def directory(request):
-    encoded_imgs = get_encoded_images(resize_x=100, resize_y=100)
+    img_state.CURRENT_PAGE = 1
 
-    context = {"images" : encoded_imgs}
+    encoded_imgs = get_encoded_images()
+
+    pages = range(1, math.ceil(img_state.TOTAL_IMG_NUM/img_state.IMAGES_PER_PAGE)+1)
+
+    context = {"images" : encoded_imgs, "pages": pages, "current_page": img_state.CURRENT_PAGE}
+
     return render(request, 'gallery.html', context)
+
+
+def page(request, *args, **kwargs):
+
+    selected_page = kwargs.get('page')
+
+    if selected_page == 'prev':
+        img_state.CURRENT_PAGE -= 1
+    elif selected_page == 'next':
+        img_state.CURRENT_PAGE += 1
+    else:
+        img_state.CURRENT_PAGE = int(selected_page)
+
+    encoded_imgs = get_encoded_images()
+
+    pages = range(1, math.ceil(img_state.TOTAL_IMG_NUM/img_state.IMAGES_PER_PAGE)+1)
+
+    context = {"images" : encoded_imgs, "pages": pages, "current_page": img_state.CURRENT_PAGE}
+
+    return render(request, 'gallery.html', context)
+
 
 def select(request, *args, **kwargs):
     selected_img = kwargs.get('img')
 
-    encoded_imgs = get_encoded_images()
+    if len(img_state.IMAGES) == 0:
+        encoded_imgs = get_encoded_images()
+    else:
+        encoded_imgs = img_state.IMAGES
 
     context = {'images' : encoded_imgs, 'selected_img': selected_img}
     return render(request, 'carousel.html', context)
 
 
-def get_encoded_images(resize_x=None, resize_y=None):
-    image_dir = STATIC_ROOT + "/img/"
 
+def get_encoded_images(resize_x=None, resize_y=None):
+    #image_dir = "/var/www/html/geodude/gallery/static/img/"
+    image_dir = '/Users/Drew/drew/geodude/geodude/gallery/static/img/'
     files = os.listdir(image_dir)
 
     # filter images by file extenstion
@@ -37,27 +79,16 @@ def get_encoded_images(resize_x=None, resize_y=None):
 
     # sort images by file name
     imgs = sorted(imgs)
+    img_state.TOTAL_IMG_NUM = len(imgs)
+
+    # filter by page
+    imgs = imgs[(img_state.CURRENT_PAGE-1)*img_state.IMAGES_PER_PAGE:img_state.CURRENT_PAGE*img_state.IMAGES_PER_PAGE]
 
     encoded_imgs = []
 
     for img in imgs:
-        image = cv2.imread(image_dir + img)
-
-        if resize_x and resize_y:
-            # resize image
-            resized = cv2.resize(image, (resize_x, resize_y))
-        else:
-            # downsize image for full screen
-            (h, w) = image.shape[:2]
-            if h > w :
-                dim = (900, 1440)
-            else:
-                dim = (1440, 900)
-
-            resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-
-        buffer = cv2.imencode('.jpg', resized)[1] # convert to numpy array
-        b64 = base64.b64encode(buffer).decode('ascii') # convert to base64
+        with open(image_dir+img, 'rb') as f:
+            b64 = base64.b64encode(f.read()).decode('ascii')
 
         encoded_imgs.append(
             dict(
@@ -66,4 +97,5 @@ def get_encoded_images(resize_x=None, resize_y=None):
             )
         )
 
+    img_state.IMAGES = encoded_imgs
     return encoded_imgs
